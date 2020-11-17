44e04e541373ee9c30a0350e81e4cda7c7eb24d5
Kak'a Chinh <chinh.bui@orientsoftware.com>
2020-11-17 17:02:27 +0700

    init

---
+++ KaercherNet.API/Controllers/CustomersController.cs
@@ -0,0 +1,32 @@
+﻿using KaercherNet.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KaercherNet.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController: ControllerBase
+    {
+        private readonly KaercherNetDbContext _context;
+        public CustomersController(KaercherNetDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(_context.Customers);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer([FromBody] Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ KaercherNet.API/KaercherNetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using KaercherNet.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KaercherNet.API
+{
+    public class KaercherNetDbContext : DbContext
+    {
+        public KaercherNetDbContext(DbContextOptions<KaercherNetDbContext> options) :base (options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}

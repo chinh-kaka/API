412dc9bd7fc24143d76b46e4487f3d54e7bbe0d9
Kak'a Chinh <chinh.bui@orientsoftware.com>
2020-11-18 11:54:43 +0700

    add swagger ui

--- KaercherNet.API/Startup.cs
+++ KaercherNet.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -5,6 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
 
 namespace KaercherNet.API
 {
@@ -25,10 +30,36 @@ namespace KaercherNet.API
                 options.Authority = "http://localhost:5000";
                 options.Audience = "kaerchernetApi";
                 options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters() { ValidateAudience = false };
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = false
+                };
             });
             services.AddDbContext<KaercherNetDbContext>(options => options.UseInMemoryDatabase("KaercherNetDbContext"));
             services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "KaercherNet API", Version = "v1" });
+               
+                //options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                //{
+                //    Type = SecuritySchemeType.OAuth2,
+                //    Flows = new OpenApiOAuthFlows
+                //    {
+                //        Implicit = new OpenApiOAuthFlow
+                //        {
+                //            AuthorizationUrl = new Uri("http://localhost:5000/connect/authorize"),
+                //            TokenUrl = new Uri("http://localhost:5000/connect/token"),
+                //            Scopes = new Dictionary<string, string>
+                //            {
+                //                { "kaerchernetApi", "kaerchernetApi" }
+                //            }
+                //        }
+                //    }
+
+                //});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +69,7 @@ namespace KaercherNet.API
             {
                 app.UseDeveloperExceptionPage();
             }
-           
+
             app.UseRouting();
 
             app.UseAuthentication();
@@ -47,6 +78,11 @@ namespace KaercherNet.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "KaercherNet API");
             });
         }
     }
